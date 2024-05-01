bc61a2f7be7770806a4372d99375a69e514ae3fa
Duarte Cecílio <cecilstorm@gmail.com>
2024-05-02 00:01:18 +0100

    dedicated function for single file conversion

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use std::path::PathBuf;
 
 use polars::prelude::LazyFrame;
 
-fn main() {
-    let output_path_string = "output/output.csv";
-    let output_path = PathBuf::from(output_path_string);
+fn parquet_to_csv(input_path: &str, output_path: &str) {
+    let output_path = PathBuf::from(&output_path);
 
-    let lf = LazyFrame::scan_parquet( "input/pandas_dataframe_test.parquet.gzip", Default::default()).unwrap();
+    let lf = LazyFrame::scan_parquet( &input_path, Default::default()).unwrap();
         
     lf.sink_csv(output_path, Default::default()).unwrap();
     println!("Parquet file successfully converted!");
+}
+
+fn main() {
+    let input_path_string = "input/pandas_dataframe_test.parquet.gzip";
+    let output_path_string = "output/output.csv";
+    
+    parquet_to_csv(&input_path_string, &output_path_string);
 }
