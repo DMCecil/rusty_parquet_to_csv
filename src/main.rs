621070d974dec9a6b569ee86b79aa677c3e2a176
Duarte Cecílio <cecilstorm@gmail.com>
2024-05-03 11:57:28 +0100

    including directory walking and progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,67 @@
+use std::fs;
+use std::io::Error;
 use std::path::PathBuf;
+use std::time::Instant; // Import the 'Instant' type for timekeeping
 
+use indicatif::{ProgressBar, ProgressStyle};
 use polars::prelude::LazyFrame;
+use rayon::prelude::*;
+use walkdir::WalkDir;
 
-fn parquet_to_csv(input_path: &str, output_path: &str) {
-    let output_path = PathBuf::from(&output_path);
+fn parquet_to_csv(input_path: &PathBuf, output_path: &PathBuf) -> Result<(), Error> {
+    let output_path = output_path.join(input_path.file_stem().unwrap()).with_extension("csv");
+
+    let start_time = Instant::now(); // Record start time
 
     let lf = LazyFrame::scan_parquet( &input_path, Default::default()).unwrap();
-        
+    let parquet_read_time = start_time.elapsed(); // Time after reading Parquet
+    
+    let conversion_start_time = Instant::now(); // Start time for CSV conversion
     lf.sink_csv(output_path, Default::default()).unwrap();
+
+    let csv_conversion_time = conversion_start_time.elapsed(); // Time after CSV conversion
+
     println!("Parquet file successfully converted!");
+    println!("Parquet reading time: {:.2?}", parquet_read_time);
+    println!("CSV conversion time: {:.2?}", csv_conversion_time);
+
+    Ok(())
 }
 
-fn main() {
-    let input_path_string = "input/pandas_dataframe_test.parquet.gzip";
-    let output_path_string = "output/output.csv";
+fn main() -> Result<(), Error> {
+    let input_dir = "input/";
+    let output_dir = "output/";
     
-    parquet_to_csv(&input_path_string, &output_path_string);
+    // Find all .parquet.gzip files in the directory
+    let parquet_files = WalkDir::new(input_dir)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| {
+            entry
+                .file_name()
+                .to_str()
+                .map(|s| s.ends_with(".parquet.gzip"))  
+                .unwrap_or(false)
+        })
+        .map(|entry| entry.path().to_path_buf())
+        .collect::<Vec<_>>();
+
+    // Create a progress bar 
+    let pb = ProgressBar::new(parquet_files.len() as u64);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
+        .unwrap()
+        .progress_chars("#>-"));
+
+    // Convert files in parallel, updating the progress bar
+    let output_dir_path = PathBuf::from(output_dir);
+    parquet_files.par_iter().for_each(|parquet_file| {
+        if let Err(e) = parquet_to_csv(parquet_file, &output_dir_path) {
+            eprintln!("Error converting {}: {}", parquet_file.display(), e)
+        }
+        pb.inc(1);
+    });
+
+    pb.finish_with_message("Conversion Complete!");
+    Ok(())
 }
